7d740a1be196b7177abc794d646e592321bad04d
cristianodelgado <cristiandelgado061996@gmail.com>
2024-04-30 22:53:10 -0300

    1.6

--- wdd230/scripts/getdates.js
+++ wdd230/scripts/getdates.js
@@ -2,3 +2,4 @@ let text = "Last Modification: " + document.lastModified;
 document.getElementById("lastModified").innerHTML = text;
 let nyear = new Date();
 document.getElementById("year").innerHTML = nyear.getFullYear();
+/**/
