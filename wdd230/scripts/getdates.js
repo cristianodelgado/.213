7bf4574759bc1eb9bba346a62bc811ebbb6efd3b
cristianodelgado <cristiandelgado061996@gmail.com>
2024-05-10 23:03:16 -0300

    3.8

--- wdd230/scripts/getdates.js
+++ wdd230/scripts/getdates.js
@@ -11,3 +11,31 @@ hamButton.addEventListener('click', () => {
     navigation.classList.toggle('open');
     hamButton.classList.toggle('open');
 });
+//-------NUMBER OF VISIT------
+
+const msToDays = 84600000;
+
+const dayVisitDisplay = document.querySelector(".today");
+let dayVisits = Date.now(window.localStorage.getItem(".stoday")) || 0;
+
+const visitsDisplay = document.querySelector(".visits");
+let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
+if (numVisits !== 0) {
+    visitsDisplay.textContent = numVisits;
+} else if (numVisits == 0) {
+
+    visitsDisplay.textContent = "Welcome!";
+}
+else if (dayVisits < msToDays) {
+    dayVisitDisplay.textContent = dayVisits;
+    dayVisitDisplay.textContent = "Back so soon! Awesome!"
+}
+else { }
+
+
+//if (dayVisits > 0) { dayVisitDisplay.textContent = dayVisits }
+
+
+
+numVisits++;
+localStorage.setItem("numVisits-ls", numVisits);
